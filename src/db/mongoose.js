8941712d5db9b2affd9259186fffb97108f96066
MuhammadTalib <talibwaseem135@gmail.com>
2020-03-19 00:00:58 -0700

    task manager

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
@@ -7,44 +6,44 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useUnifiedTopology: true
 })
 
-const User = mongoose.model('User',{
-    name:{
-        type:String,
-        required:true,
-        trim:true
-    },
-    email:{
-        type:String,
-        required: true,
-        trim: true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:7,
-        trim:true,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
-                throw new Error('Password connot contain passoword')
-            }
-        }
-    },
-    age:{
-        type:Number,
-        default:0,
-        validate(value){
-            if(value<0){
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }   
-})
+// const User = mongoose.model('User',{
+//     name:{
+//         type:String,
+//         required:true,
+//         trim:true
+//     },
+//     email:{
+//         type:String,
+//         required: true,
+//         trim: true,
+//         lowercase:true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Email is invalid')
+//             }
+//         }
+//     },
+//     password:{
+//         type:String,
+//         required:true,
+//         minlength:7,
+//         trim:true,
+//         validate(value){
+//             if(value.toLowerCase().includes('password')){
+//                 throw new Error('Password connot contain passoword')
+//             }
+//         }
+//     },
+//     age:{
+//         type:Number,
+//         default:0,
+//         validate(value){
+//             if(value<0){
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+//     }   
+// })
 
 // const me = new User({
 //     name: '         TALIB WASEEM      ',
@@ -58,25 +57,25 @@ const User = mongoose.model('User',{
 //     console.log(error.message)
 // })
 
-const Task = mongoose.model('Task',{
-    description:{
-        type:String,
-        required: true,
-        trim:true,
+// const Task = mongoose.model('Task',{
+//     description:{
+//         type:String,
+//         required: true,
+//         trim:true,
 
-    },
-    completed:{
-        type: Boolean,
-        default:false
-    }
-})
+//     },
+//     completed:{
+//         type: Boolean,
+//         default:false
+//     }
+// })
 
-const task = new Task({
-    description: '       Learn the Mongoose Library',
-})
+// const task = new Task({
+//     description: '       Learn the Mongoose Library',
+// })
 
-task.save().then(()=>{
-    console.log(task)
-}).catch((error)=>{
-    console.log(error)
-})
+// task.save().then(()=>{
+//     console.log(task)
+// }).catch((error)=>{
+//     console.log(error)
+// })
