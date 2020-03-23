5b0a01247d4528336dfe79b4a7b0228e1fea746c
MuhammadTalib <talibwaseem135@gmail.com>
2020-03-22 21:55:27 -0700

    JWT

--- src/models/task.js
+++ src/models/task.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose')
 
 const Task = mongoose.model('Task',{
--- src/routers/user.js
+++ src/routers/user.js
@@ -5,20 +5,31 @@ const router = new express.Router()
 router.post('/users',async (req,res)=>{
     const user = new User(req.body)
     
-    // try{
-    //     console.log(user)
-    //     await user.save()
-    //     res.status(201).send(user)
-    // }catch(e){
-    //     res.status(400).send(e)
-    // }
-
-    user.save().then(()=>{
-        res.send(user)
-    }).catch((e)=>{
+    try{
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({user,token})
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
+
+    // user.save().then(()=>{
+    //     res.send(user)
+    // }).catch((e)=>{
+    //     res.status(400).send(e)
+    // })
 })
+
+router.post('/users/login',async (req,res)=>{
+    try{
+        const user = await User.findByCredentials(req.body.email,req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({user,token})
+    }catch(e){
+        res.status(400).send();
+    }
+})
+
 
 router.get('/users',async (req,res)=>{
 
@@ -44,9 +55,9 @@ router.get('/users/:id',async (req,res)=>{
         if(!user){
             return res.status(404).send();
         }
-        res.send(user)
+        res.send(user);
     }catch(e){
-        res.send(500).send();
+        res.status(500).send();
     }
  
     // User.findById(_id).then((user)=>{
@@ -71,7 +82,15 @@ router.patch('/users/:id',async(req,res)=>{
     }
 
     try{
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators: true}) 
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update)=>{
+            user[update]=req.body[update];
+        })
+
+        await user.save()
+
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators: true}) 
         if(!user){
            return res.status(404).send(e)
         }
