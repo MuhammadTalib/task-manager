dd210f423a6cac79172cb1e91e00b1def84f9577
MuhammadTalib <talibwaseem135@gmail.com>
2020-03-23 10:06:07 -0700

    express middlewares

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const User = require('../models/user')
 const router = new express.Router()
-
+const auth = require('../middleware/auth')
 router.post('/users',async (req,res)=>{
     const user = new User(req.body)
     
@@ -31,20 +31,15 @@ router.post('/users/login',async (req,res)=>{
 })
 
 
-router.get('/users',async (req,res)=>{
+router.get('/users/me',auth,async (req,res)=>{
 
+    //res.send(req.user)
     try{
         const users = await User.find({})
         res.send(users)
     }catch(e){
         res.status(500).send()
     }
-
-    // User.find({}).then((users)=>{
-    //     res.send(users)
-    // }).catch((e=>{
-    //     res.status(500).send()
-    // }))
 })
 
 router.get('/users/:id',async (req,res)=>{
