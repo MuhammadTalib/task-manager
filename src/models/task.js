80f7d4b09a0795b009e167958a2ae4b55f9f98f6
MuhammadTalib <talibwaseem135@gmail.com>
2020-03-25 02:40:30 -0700

    sorting,pagination,timestamp,filtering

--- src/models/task.js
+++ src/models/task.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task',{
+const taskSchema=new mongoose.Schema ({
     description:{
         type:String,
         required: true,
@@ -17,5 +17,9 @@ const Task = mongoose.model('Task',{
         required: true,
         ref: 'User'
     }
-});
-module.exports = Task;
+},{
+    timestamps:true
+})
+
+const Task = mongoose.model('Task',taskSchema);
+module.exports = Task; 
