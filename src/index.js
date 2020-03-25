7fdfe6b00ad871c0d3b6229bb73d1c3a80a4429f
MuhammadTalib <talibwaseem135@gmail.com>
2020-03-25 13:00:06 -0700

    Image and File Upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 const express = require('express') 
 require('./db/mongoose')
-const User = require("./models/user")
-// const Task = require("./models/task")
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)  
+
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
+})
+
+// const multer = require('multer')
+// const upload = multer({
+//     dest: 'images'
+// })
+// app.post('/upload',upload.single('upload') ,(req,res)=>{
+//     res.send() 
+// })
+
 
 // app.use((req,res,next)=>{
 //     if(req.method==='GET'){
@@ -20,13 +35,6 @@ const port = process.env.PORT || 3000;
 //     res.status(503).send('Site is currently down. cHECK back soon')
 // })
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)  
-
-app.listen(port,()=>{
-    console.log('Server is up on port '+port)
-})
 
 // const Task = require('./models/task')
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,9 @@
 const express = require('express') 
 const User = require('../models/user')
 const router = new express.Router()
+const sharp = require('sharp')
 const auth = require('../middleware/auth')
+const multer = require('multer')
 
 router.post('/users',async (req,res)=>{
     const user = new User(req.body)
@@ -22,11 +24,8 @@ router.post('/users',async (req,res)=>{
 })
 
 router.post('/users/login',async (req,res)=>{
-    console.log("logging")
     try{
-        console.log("trying")
         const user = await User.findByCredentials(req.body.email,req.body.password)
-        console.log("user",user)
         const token = await user.generateAuthToken()
         res.send({user:user,token})
     }catch(e){
@@ -131,6 +130,55 @@ router.delete('/users/me',auth,async(req,res)=>{
         res.send(req.user)
     }catch(e){ 
         return res.status(500).send()
+    }
+})
+const upload = multer({
+    // dest:'avatars',
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload an Image'))
+        }
+        cb(undefined,true)
+
+
+        // cb(new Error('File must be a pdf'))
+        // cb(undefined,false)
+
+    }
+})
+router.post('/users/me/avatar',auth, upload.single('avatar'),async (req,res)=>{
+   
+    const buffer = await sharp(req.file.buffer).resize({ width:250, height:250}).png().toBuffer() 
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+},(error,req,res,next)=>{
+    res.status(400).send({
+        error:error.message
+    })
+})
+
+router.delete('/users/me/avatar',auth,async (req,res)=>{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type','image/png')
+        res.send(user.avatar)
+
+    }catch(e){
+        res.status(404).send()
     }
 })
 
